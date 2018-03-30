4d5a0d6419fe8d36cb251d2a7db413fc04f91014
aleclarson <alec.stanford.larson@gmail.com>
2018-03-30 14:22:27 -0400

    remove 'http.client' module

--- src/lua/init.c
+++ src/lua/init.c
@@ -56,7 +56,6 @@
 #include "lua/msgpack.h"
 #include "lua/pickle.h"
 #include "lua/fio.h"
-#include "lua/httpc.h"
 #include "digest.h"
 #include <small/ibuf.h>
 
@@ -87,7 +86,6 @@ extern char strict_lua[],
 	buffer_lua[],
 	errno_lua[],
 	fiber_lua[],
-	httpc_lua[],
 	log_lua[],
 	uri_lua[],
 	socket_lua[],
@@ -144,7 +142,6 @@ static const char *lua_modules[] = {
 	"internal.argparse", argparse_lua,
 	"internal.trigger", trigger_lua,
 	"pwd", pwd_lua,
-	"http.client", httpc_lua,
 	"iconv", iconv_lua,
 	/* jit.* library */
 	"jit.vmdef", vmdef_lua,
@@ -408,8 +405,6 @@ tarantool_lua_init(const char *tarantool_bin, int argc, char **argv)
 	tarantool_lua_socket_init(L);
 	tarantool_lua_pickle_init(L);
 	tarantool_lua_digest_init(L);
-	luaopen_http_client_driver(L);
-	lua_pop(L, 1);
 	luaopen_msgpack(L);
 	lua_pop(L, 1);
 	luaopen_yaml(L);
